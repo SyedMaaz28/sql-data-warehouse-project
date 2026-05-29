54a2f3b95189c144ff99bf9f8a17d03dce5a2bb9
Syed Maaz <106867059+SyedMaaz28@users.noreply.github.com>
2026-05-29 18:53:21 +0530

    Create DB DataWarehouse and schemas
    This script initializes a new database and creates three schemas for the Medalion Architecture.

---
+++ Scripts/init_database.sql
@@ -0,0 +1,8 @@
+-- Create database
+CREATE DATABASE DataWarehouse;
+
+-- Creating Schema  ----> As we are following a Medalion Architecture we are creating 3 Schemas
+
+CREATE SCHEMA bronze;
+CREATE SCHEMA silver;
+CREATE SCHEMA gold;
